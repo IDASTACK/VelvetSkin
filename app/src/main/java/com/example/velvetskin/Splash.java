752d6587be9df73ded810769d562342fc304abd8
itzelmun <isanchez17@ucol.mx>
2021-08-30 23:43:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/velvetskin/Splash.java
@@ -0,0 +1,25 @@
+package com.example.velvetskin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Splash.this, Login.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+    }
+
+}
